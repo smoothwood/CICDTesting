5910caf00b51001f96b8789f17b4ceb0dffafeef
YCM <ycm@outlook.com>
2020-03-14 11:23:51 +0800

    inital commit

---
+++ CICDTesting.Unit.Tests/UnitTest1.cs
@@ -0,0 +1,28 @@
+using System;
+using Xunit;
+
+namespace CICDTesting.Unit.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //Arrange
+            int a = 10;
+
+            //Act
+
+            //Assert
+            Assert.Equal(10, a);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int b = 100;
+
+            Assert.Equal(10, b);
+        }
+    }
+}
---
+++ CICDTesting/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CICDTesting.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
