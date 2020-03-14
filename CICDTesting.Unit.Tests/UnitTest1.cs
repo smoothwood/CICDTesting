7b1f04810b843727f0f446734918c73ca2c3f0eb
YCM <ycm@outlook.com>
2020-03-14 18:44:15 +0800

    modified

--- CICDTesting.Unit.Tests/UnitTest1.cs
+++ CICDTesting.Unit.Tests/UnitTest1.cs
@@ -20,7 +20,7 @@ namespace CICDTesting.Unit.Tests
         [Fact]
         public void Test2()
         {
-            int b = 103;
+            int b = 104;
 
             Assert.Equal(10, b);
         }
