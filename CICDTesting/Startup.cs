e30ea962e179361204a1982724fc642a1a4c7c5e
YCM <ycm@outlook.com>
2020-03-17 16:02:16 +0800

    updated

--- CICDTesting/Startup.cs
+++ CICDTesting/Startup.cs
@@ -49,7 +49,7 @@ namespace CICDTesting
                 app.UseHsts();
             }
 
-            // app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
