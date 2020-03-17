a0f21a8037fa84a533379108cabc6d2c2e260fa0
YCM <ycm@outlook.com>
2020-03-17 15:51:41 +0800

    updated

--- CICDTesting/Startup.cs
+++ CICDTesting/Startup.cs
@@ -40,10 +40,7 @@ namespace CICDTesting
         {
             if (env.IsDevelopment())
             {
-                // app.UseDeveloperExceptionPage();
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseDeveloperExceptionPage();
             }
             else
             {
@@ -52,7 +49,7 @@ namespace CICDTesting
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
